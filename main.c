e3d32859111bbb52f88608caef1487d388ef4610
LingrowS <117826958+LingrowS@users.noreply.github.com>
2026-02-14 19:02:36 +0100

    Refactor main function and menu handling
    Refactor main function to improve structure and readability.

--- main.c
+++ main.c
@@ -1,10 +1,56 @@
+// main.c
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "gamelib.h"
 
-// Questo file continene solo la definizione della funzione main
-// con il menu principale 1-2-3
+//Funzione main, tutto ha inizio da qui
+int main(void) {
+    int scelta = 0;
 
-int main( ) {
+    printf("\n--- BENVENUTI A OCCHINZ ---\n");
 
+    do {
+        printf("MENU PRINCIPALE:\n");
+        printf("1) Imposta gioco\n");
+        printf("2) Gioca\n");
+        printf("3) Termina gioco\n");
+        printf("4) Crediti\n");
+        printf("Inserisci la tua scelta: ");
 
+        //scanf salta automaticamente tab, spazi e /n
+        if (scanf("%d", &scelta) != 1) { //scanf con %d ritorna 1 quando legge un intero, torna 0 se non può convertire ad intero (es: CIAO, 12Az)
+            // In questa zona gestiamo l'input non numerico
+            scelta = 0;//impostiamo la scelta ad un valore che non ci fa proseguire(0 in questo caso non corrisponde a nessun input utile)
+        }
+        
+        //Puliamo il buffer da eventuali residui
+        while (getchar() != '\n');
 
+        switch (scelta) {
+            case 1:
+                system("clear");
+                imposta_gioco();
+                break;
+            case 2:
+                system("clear");
+                gioca();
+                break;
+            case 3:
+                system("clear");
+                termina_gioco();
+                break;
+            case 4:
+                system("clear");
+                crediti();
+                break;
+            default:
+                system("clear");
+                printf("\n[ERRORE] Scelta non valida. Riprova.\n");
+                break;
+        }
+
+    } while (scelta != 3);
+
+    return 0;
 }
